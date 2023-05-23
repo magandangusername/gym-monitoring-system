ba9b00158581b2e9804bbe51bf5b833466b1a648
Skeppy <Skeppy@DESKTOP-N221KKM>
2023-05-24 04:06:04 +0800

    merge

--- Gym Monitoring System/profile.vb
+++ Gym Monitoring System/profile.vb
@@ -1,11 +1,21 @@
-﻿Public Class profile
+﻿Imports System.Data.OleDb
+Public Class profile
     Private Sub btnBack_Click(sender As Object, e As EventArgs) Handles btnBack.Click
         member_dashboard.Show()
         Me.Hide()
     End Sub
 
     Private Sub profile_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'load the current user profile info to the screen
+        Call DBConnection.con.Open()
+        Dim MemberProfile As New OleDbCommand("SELECT member_id FROM Members ORDER BY member_id DESC", con)
+        Dim getdata As OleDbDataReader
+        getdata = MemberProfile.ExecuteReader
+        getdata.Read()
+        txtFullname.Text = ""
 
+        getdata.Close()
+        Call DBConnection.con.Close()
     End Sub
 
     Private Sub txtHeight_KeyPress(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyPressEventArgs) Handles txtHeight.KeyPress
