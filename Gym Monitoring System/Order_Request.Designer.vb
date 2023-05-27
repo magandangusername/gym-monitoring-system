3126469302b6b1f85d415a04dc57034c5624a928
Matt <mount.olympus.17@gmail.com>
2023-05-27 14:02:17 +0800

    design membership and reciept

--- Gym Monitoring System/Order_Request.Designer.vb
+++ Gym Monitoring System/Order_Request.Designer.vb
@@ -22,6 +22,7 @@ Partial Class Order_Request
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Order_Request))
         Me.Label1 = New System.Windows.Forms.Label()
         Me.GyM_DBDataSet1 = New Gym_Monitoring_System.GYM_DBDataSet()
         Me.Button1 = New System.Windows.Forms.Button()
@@ -41,15 +42,22 @@ Partial Class Order_Request
         Me.lblMembership = New System.Windows.Forms.Label()
         Me.Label6 = New System.Windows.Forms.Label()
         Me.lblOrderID = New System.Windows.Forms.Label()
+        Me.PictureBox6 = New System.Windows.Forms.PictureBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         CType(Me.GyM_DBDataSet1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(36, 39)
+        Me.Label1.BackColor = System.Drawing.Color.White
+        Me.Label1.Font = New System.Drawing.Font("Courier New", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(36, 119)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(78, 13)
+        Me.Label1.Size = New System.Drawing.Size(104, 16)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Account Name"
         '
@@ -60,162 +68,232 @@ Partial Class Order_Request
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(139, 288)
+        Me.Button1.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Button1.Font = New System.Drawing.Font("Copperplate Gothic Bold", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(173, 352)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(129, 23)
         Me.Button1.TabIndex = 1
         Me.Button1.Text = "Print Request"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.Button1.UseVisualStyleBackColor = False
         '
         'lblCurrDateTime
         '
         Me.lblCurrDateTime.AutoSize = True
-        Me.lblCurrDateTime.Location = New System.Drawing.Point(299, 13)
+        Me.lblCurrDateTime.BackColor = System.Drawing.Color.White
+        Me.lblCurrDateTime.Font = New System.Drawing.Font("Courier New", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblCurrDateTime.Location = New System.Drawing.Point(333, 93)
         Me.lblCurrDateTime.Name = "lblCurrDateTime"
-        Me.lblCurrDateTime.Size = New System.Drawing.Size(86, 13)
+        Me.lblCurrDateTime.Size = New System.Drawing.Size(136, 16)
         Me.lblCurrDateTime.TabIndex = 2
         Me.lblCurrDateTime.Text = "Current Datetime"
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(36, 74)
+        Me.Label3.BackColor = System.Drawing.Color.White
+        Me.Label3.Font = New System.Drawing.Font("Courier New", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(76, 151)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(45, 13)
+        Me.Label3.Size = New System.Drawing.Size(64, 16)
         Me.Label3.TabIndex = 3
         Me.Label3.Text = "Address"
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(36, 105)
+        Me.Label4.BackColor = System.Drawing.Color.White
+        Me.Label4.Font = New System.Drawing.Font("Courier New", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(20, 185)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(84, 13)
+        Me.Label4.Size = New System.Drawing.Size(120, 16)
         Me.Label4.TabIndex = 4
         Me.Label4.Text = "Contact Number"
         '
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(36, 167)
+        Me.Label5.BackColor = System.Drawing.Color.White
+        Me.Label5.Font = New System.Drawing.Font("Courier New", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(92, 247)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(31, 13)
+        Me.Label5.Size = New System.Drawing.Size(48, 16)
         Me.Label5.TabIndex = 5
         Me.Label5.Text = "Price"
         '
         'Label8
         '
         Me.Label8.AutoSize = True
-        Me.Label8.Location = New System.Drawing.Point(39, 221)
+        Me.Label8.BackColor = System.Drawing.Color.White
+        Me.Label8.Font = New System.Drawing.Font("Courier New", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(76, 301)
         Me.Label8.Name = "Label8"
-        Me.Label8.Size = New System.Drawing.Size(37, 13)
+        Me.Label8.Size = New System.Drawing.Size(64, 16)
         Me.Label8.TabIndex = 8
         Me.Label8.Text = "Total: "
         '
         'Label9
         '
         Me.Label9.AutoSize = True
-        Me.Label9.Location = New System.Drawing.Point(38, 194)
+        Me.Label9.BackColor = System.Drawing.Color.White
+        Me.Label9.Font = New System.Drawing.Font("Courier New", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(28, 274)
         Me.Label9.Name = "Label9"
-        Me.Label9.Size = New System.Drawing.Size(71, 13)
+        Me.Label9.Size = New System.Drawing.Size(112, 16)
         Me.Label9.TabIndex = 9
         Me.Label9.Text = "Discount in %"
         '
         'lblDiscounted
         '
         Me.lblDiscounted.AutoSize = True
-        Me.lblDiscounted.Location = New System.Drawing.Point(136, 221)
+        Me.lblDiscounted.BackColor = System.Drawing.Color.White
+        Me.lblDiscounted.Font = New System.Drawing.Font("Courier New", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDiscounted.Location = New System.Drawing.Point(146, 301)
         Me.lblDiscounted.Name = "lblDiscounted"
-        Me.lblDiscounted.Size = New System.Drawing.Size(13, 13)
+        Me.lblDiscounted.Size = New System.Drawing.Size(16, 16)
         Me.lblDiscounted.TabIndex = 11
         Me.lblDiscounted.Text = "0"
         '
         'lblPrice
         '
         Me.lblPrice.AutoSize = True
-        Me.lblPrice.Location = New System.Drawing.Point(136, 167)
+        Me.lblPrice.BackColor = System.Drawing.Color.White
+        Me.lblPrice.Font = New System.Drawing.Font("Courier New", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPrice.Location = New System.Drawing.Point(146, 247)
         Me.lblPrice.Name = "lblPrice"
-        Me.lblPrice.Size = New System.Drawing.Size(13, 13)
+        Me.lblPrice.Size = New System.Drawing.Size(16, 16)
         Me.lblPrice.TabIndex = 12
         Me.lblPrice.Text = "0"
         '
         'lblDiscount
         '
         Me.lblDiscount.AutoSize = True
-        Me.lblDiscount.Location = New System.Drawing.Point(136, 194)
+        Me.lblDiscount.BackColor = System.Drawing.Color.White
+        Me.lblDiscount.Font = New System.Drawing.Font("Courier New", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDiscount.Location = New System.Drawing.Point(146, 274)
         Me.lblDiscount.Name = "lblDiscount"
-        Me.lblDiscount.Size = New System.Drawing.Size(13, 13)
+        Me.lblDiscount.Size = New System.Drawing.Size(16, 16)
         Me.lblDiscount.TabIndex = 13
         Me.lblDiscount.Text = "0"
         '
         'lblName
         '
         Me.lblName.AutoSize = True
-        Me.lblName.Location = New System.Drawing.Point(136, 39)
+        Me.lblName.BackColor = System.Drawing.Color.White
+        Me.lblName.Font = New System.Drawing.Font("Courier New", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblName.Location = New System.Drawing.Point(146, 119)
         Me.lblName.Name = "lblName"
-        Me.lblName.Size = New System.Drawing.Size(38, 13)
+        Me.lblName.Size = New System.Drawing.Size(40, 16)
         Me.lblName.TabIndex = 14
         Me.lblName.Text = "NAME"
         '
         'lblAddress
         '
         Me.lblAddress.AutoSize = True
-        Me.lblAddress.Location = New System.Drawing.Point(136, 74)
+        Me.lblAddress.BackColor = System.Drawing.Color.White
+        Me.lblAddress.Font = New System.Drawing.Font("Courier New", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAddress.Location = New System.Drawing.Point(146, 151)
         Me.lblAddress.Name = "lblAddress"
-        Me.lblAddress.Size = New System.Drawing.Size(59, 13)
+        Me.lblAddress.Size = New System.Drawing.Size(64, 16)
         Me.lblAddress.TabIndex = 14
         Me.lblAddress.Text = "ADDRESS"
         '
         'lblContact
         '
         Me.lblContact.AutoSize = True
-        Me.lblContact.Location = New System.Drawing.Point(136, 105)
+        Me.lblContact.BackColor = System.Drawing.Color.White
+        Me.lblContact.Font = New System.Drawing.Font("Courier New", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblContact.Location = New System.Drawing.Point(146, 185)
         Me.lblContact.Name = "lblContact"
-        Me.lblContact.Size = New System.Drawing.Size(108, 13)
+        Me.lblContact.Size = New System.Drawing.Size(120, 16)
         Me.lblContact.TabIndex = 14
         Me.lblContact.Text = "CONTACT NUMBER"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(38, 140)
+        Me.Label2.BackColor = System.Drawing.Color.White
+        Me.Label2.Font = New System.Drawing.Font("Courier New", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(52, 219)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(64, 13)
+        Me.Label2.Size = New System.Drawing.Size(88, 16)
         Me.Label2.TabIndex = 15
         Me.Label2.Text = "Membership"
         '
         'lblMembership
         '
         Me.lblMembership.AutoSize = True
-        Me.lblMembership.Location = New System.Drawing.Point(136, 140)
+        Me.lblMembership.BackColor = System.Drawing.Color.White
+        Me.lblMembership.Font = New System.Drawing.Font("Courier New", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblMembership.Location = New System.Drawing.Point(146, 219)
         Me.lblMembership.Name = "lblMembership"
-        Me.lblMembership.Size = New System.Drawing.Size(79, 13)
+        Me.lblMembership.Size = New System.Drawing.Size(88, 16)
         Me.lblMembership.TabIndex = 15
         Me.lblMembership.Text = "MEMBERSHIP"
         '
         'Label6
         '
         Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(39, 13)
+        Me.Label6.BackColor = System.Drawing.Color.White
+        Me.Label6.Font = New System.Drawing.Font("Courier New", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(68, 93)
         Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(47, 13)
+        Me.Label6.Size = New System.Drawing.Size(72, 16)
         Me.Label6.TabIndex = 16
         Me.Label6.Text = "Order ID"
         '
         'lblOrderID
         '
         Me.lblOrderID.AutoSize = True
-        Me.lblOrderID.Location = New System.Drawing.Point(136, 13)
+        Me.lblOrderID.BackColor = System.Drawing.Color.White
+        Me.lblOrderID.Font = New System.Drawing.Font("Courier New", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblOrderID.Location = New System.Drawing.Point(146, 93)
         Me.lblOrderID.Name = "lblOrderID"
-        Me.lblOrderID.Size = New System.Drawing.Size(55, 13)
+        Me.lblOrderID.Size = New System.Drawing.Size(72, 16)
         Me.lblOrderID.TabIndex = 16
         Me.lblOrderID.Text = "00000000"
+        '
+        'PictureBox6
+        '
+        Me.PictureBox6.Image = CType(resources.GetObject("PictureBox6.Image"), System.Drawing.Image)
+        Me.PictureBox6.Location = New System.Drawing.Point(353, 21)
+        Me.PictureBox6.Name = "PictureBox6"
+        Me.PictureBox6.Size = New System.Drawing.Size(51, 45)
+        Me.PictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox6.TabIndex = 27
+        Me.PictureBox6.TabStop = False
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 21.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Label7.Location = New System.Drawing.Point(149, 26)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(198, 35)
+        Me.Label7.TabIndex = 26
+        Me.Label7.Text = "GymBros PH"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(92, 21)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(51, 45)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 25
+        Me.PictureBox1.TabStop = False
         '
         'Order_Request
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(413, 378)
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(492, 387)
+        Me.Controls.Add(Me.PictureBox6)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.lblOrderID)
         Me.Controls.Add(Me.Label6)
         Me.Controls.Add(Me.lblMembership)
@@ -237,6 +315,8 @@ Partial Class Order_Request
         Me.Name = "Order_Request"
         Me.Text = "Order_Request"
         CType(Me.GyM_DBDataSet1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -261,4 +341,7 @@ Partial Class Order_Request
     Friend WithEvents lblMembership As Label
     Friend WithEvents Label6 As Label
     Friend WithEvents lblOrderID As Label
+    Friend WithEvents PictureBox6 As PictureBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents PictureBox1 As PictureBox
 End Class
--- Gym Monitoring System/buy_membership.Designer.vb
+++ Gym Monitoring System/buy_membership.Designer.vb
@@ -22,6 +22,7 @@ Partial Class buy_membership
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(buy_membership))
         Me.Label1 = New System.Windows.Forms.Label()
         Me.rdbdiscount1 = New System.Windows.Forms.RadioButton()
         Me.rdbdiscount2 = New System.Windows.Forms.RadioButton()
@@ -31,23 +32,32 @@ Partial Class buy_membership
         Me.btnPlaceOrder = New System.Windows.Forms.Button()
         Me.lblPrice = New System.Windows.Forms.Label()
         Me.lblPriceTotal = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(86, 25)
+        Me.Label1.Font = New System.Drawing.Font("Copperplate Gothic Bold", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Cyan
+        Me.Label1.Location = New System.Drawing.Point(109, 82)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(64, 13)
+        Me.Label1.Size = New System.Drawing.Size(81, 13)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Membership"
         '
         'rdbdiscount1
         '
         Me.rdbdiscount1.AutoSize = True
-        Me.rdbdiscount1.Location = New System.Drawing.Point(36, 68)
+        Me.rdbdiscount1.Font = New System.Drawing.Font("Copperplate Gothic Bold", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rdbdiscount1.ForeColor = System.Drawing.Color.Cyan
+        Me.rdbdiscount1.Location = New System.Drawing.Point(40, 125)
         Me.rdbdiscount1.Name = "rdbdiscount1"
-        Me.rdbdiscount1.Size = New System.Drawing.Size(188, 17)
+        Me.rdbdiscount1.Size = New System.Drawing.Size(226, 17)
         Me.rdbdiscount1.TabIndex = 1
         Me.rdbdiscount1.TabStop = True
         Me.rdbdiscount1.Text = "2 Weeks - 2% Discount (PHP 750)"
@@ -56,9 +66,11 @@ Partial Class buy_membership
         'rdbdiscount2
         '
         Me.rdbdiscount2.AutoSize = True
-        Me.rdbdiscount2.Location = New System.Drawing.Point(36, 101)
+        Me.rdbdiscount2.Font = New System.Drawing.Font("Copperplate Gothic Bold", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rdbdiscount2.ForeColor = System.Drawing.Color.Cyan
+        Me.rdbdiscount2.Location = New System.Drawing.Point(40, 158)
         Me.rdbdiscount2.Name = "rdbdiscount2"
-        Me.rdbdiscount2.Size = New System.Drawing.Size(187, 17)
+        Me.rdbdiscount2.Size = New System.Drawing.Size(232, 17)
         Me.rdbdiscount2.TabIndex = 2
         Me.rdbdiscount2.TabStop = True
         Me.rdbdiscount2.Text = "1 Month 5% Discount (PHP 1,500)"
@@ -67,9 +79,11 @@ Partial Class buy_membership
         'rdbdiscount3
         '
         Me.rdbdiscount3.AutoSize = True
-        Me.rdbdiscount3.Location = New System.Drawing.Point(36, 137)
+        Me.rdbdiscount3.Font = New System.Drawing.Font("Copperplate Gothic Bold", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rdbdiscount3.ForeColor = System.Drawing.Color.Cyan
+        Me.rdbdiscount3.Location = New System.Drawing.Point(40, 194)
         Me.rdbdiscount3.Name = "rdbdiscount3"
-        Me.rdbdiscount3.Size = New System.Drawing.Size(204, 17)
+        Me.rdbdiscount3.Size = New System.Drawing.Size(252, 17)
         Me.rdbdiscount3.TabIndex = 3
         Me.rdbdiscount3.TabStop = True
         Me.rdbdiscount3.Text = "3 Months - 10% Discount (PHP 3,500)"
@@ -78,9 +92,11 @@ Partial Class buy_membership
         'rdbdiscount4
         '
         Me.rdbdiscount4.AutoSize = True
-        Me.rdbdiscount4.Location = New System.Drawing.Point(36, 173)
+        Me.rdbdiscount4.Font = New System.Drawing.Font("Copperplate Gothic Bold", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rdbdiscount4.ForeColor = System.Drawing.Color.Cyan
+        Me.rdbdiscount4.Location = New System.Drawing.Point(40, 230)
         Me.rdbdiscount4.Name = "rdbdiscount4"
-        Me.rdbdiscount4.Size = New System.Drawing.Size(204, 17)
+        Me.rdbdiscount4.Size = New System.Drawing.Size(252, 17)
         Me.rdbdiscount4.TabIndex = 4
         Me.rdbdiscount4.TabStop = True
         Me.rdbdiscount4.Text = "6 Months - 13% Discount (PHP 5,500)"
@@ -89,9 +105,11 @@ Partial Class buy_membership
         'rdbdiscount5
         '
         Me.rdbdiscount5.AutoSize = True
-        Me.rdbdiscount5.Location = New System.Drawing.Point(36, 208)
+        Me.rdbdiscount5.Font = New System.Drawing.Font("Copperplate Gothic Bold", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rdbdiscount5.ForeColor = System.Drawing.Color.Cyan
+        Me.rdbdiscount5.Location = New System.Drawing.Point(40, 265)
         Me.rdbdiscount5.Name = "rdbdiscount5"
-        Me.rdbdiscount5.Size = New System.Drawing.Size(191, 17)
+        Me.rdbdiscount5.Size = New System.Drawing.Size(235, 17)
         Me.rdbdiscount5.TabIndex = 5
         Me.rdbdiscount5.TabStop = True
         Me.rdbdiscount5.Text = "1 Year - 13% Discount (PHP 9,000)"
@@ -99,36 +117,78 @@ Partial Class buy_membership
         '
         'btnPlaceOrder
         '
-        Me.btnPlaceOrder.Location = New System.Drawing.Point(89, 288)
+        Me.btnPlaceOrder.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.btnPlaceOrder.Font = New System.Drawing.Font("Copperplate Gothic Bold", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnPlaceOrder.Location = New System.Drawing.Point(80, 360)
         Me.btnPlaceOrder.Name = "btnPlaceOrder"
-        Me.btnPlaceOrder.Size = New System.Drawing.Size(75, 23)
+        Me.btnPlaceOrder.Size = New System.Drawing.Size(133, 23)
         Me.btnPlaceOrder.TabIndex = 6
         Me.btnPlaceOrder.Text = "Place Order"
-        Me.btnPlaceOrder.UseVisualStyleBackColor = True
+        Me.btnPlaceOrder.UseVisualStyleBackColor = False
         '
         'lblPrice
         '
         Me.lblPrice.AutoSize = True
-        Me.lblPrice.Location = New System.Drawing.Point(33, 249)
+        Me.lblPrice.Font = New System.Drawing.Font("Copperplate Gothic Bold", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPrice.ForeColor = System.Drawing.Color.Cyan
+        Me.lblPrice.Location = New System.Drawing.Point(37, 306)
         Me.lblPrice.Name = "lblPrice"
-        Me.lblPrice.Size = New System.Drawing.Size(34, 13)
+        Me.lblPrice.Size = New System.Drawing.Size(44, 13)
         Me.lblPrice.TabIndex = 7
         Me.lblPrice.Text = "Price:"
         '
         'lblPriceTotal
         '
         Me.lblPriceTotal.AutoSize = True
-        Me.lblPriceTotal.Location = New System.Drawing.Point(73, 249)
+        Me.lblPriceTotal.Font = New System.Drawing.Font("Copperplate Gothic Bold", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPriceTotal.ForeColor = System.Drawing.Color.Cyan
+        Me.lblPriceTotal.Location = New System.Drawing.Point(77, 306)
         Me.lblPriceTotal.Name = "lblPriceTotal"
-        Me.lblPriceTotal.Size = New System.Drawing.Size(13, 13)
+        Me.lblPriceTotal.Size = New System.Drawing.Size(15, 13)
         Me.lblPriceTotal.TabIndex = 7
         Me.lblPriceTotal.Text = "0"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Label7.Location = New System.Drawing.Point(80, 11)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(163, 31)
+        Me.Label7.TabIndex = 29
+        Me.Label7.Text = "GymBros PH"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(33, 9)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(41, 36)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 28
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(249, 9)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(41, 36)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 30
+        Me.PictureBox2.TabStop = False
         '
         'buy_membership
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(276, 357)
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(307, 408)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.lblPriceTotal)
         Me.Controls.Add(Me.lblPrice)
         Me.Controls.Add(Me.btnPlaceOrder)
@@ -140,6 +200,8 @@ Partial Class buy_membership
         Me.Controls.Add(Me.Label1)
         Me.Name = "buy_membership"
         Me.Text = "buy_membership"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -154,4 +216,7 @@ Partial Class buy_membership
     Friend WithEvents btnPlaceOrder As Button
     Friend WithEvents lblPrice As Label
     Friend WithEvents lblPriceTotal As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
 End Class
